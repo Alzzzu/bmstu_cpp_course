606163278b7d3ddbb80153b49086db01b66498e5
Alzzzu <alisa.zagorodnikova@yandex.ru>
2025-11-12 01:25:24 +0300

    stack

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,9 +1,37 @@
 #include "int2str.h"
 #include <assert.h>
 #include "stdio.h"
+#include <limits.h>
+
+int countDigits(int n) {
+    if (n == 0) {
+        return 0; 
+    }
+    return 1 + countDigits(n / 10);
+}
 
 char* int2str(int number)
 {
-	char* str = "0";
+	if(number == INT_MIN){
+		return "-2147483648";
+	}
+    int size = countDigits(number);
+	int counter = 10;
+	char digs[size + (number<0?1:0) + 1];
+
+	if(number<0&&number!=2147483648){
+		digs[0] = '-';
+		number = -number;
+		++size;
+	}
+	digs[size] = '\0';
+	
+    do{
+		size--;
+        digs[size] = number%10+'0';
+		number/=10;
+    } while (number/10!=0||number%10!=0);
+	
+	char* str = &digs[0];
 	return str;
 }
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,8 +1,32 @@
 #include "str2int.h"
 #include <assert.h>
-#include "stdio.h"
+#include <stdio.h> 
+#include <ctype.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int str2int(const char* str)
 {
-	return 0;
+
+	int res = 0;
+	int sign = 1;
+	int hasDigs = 0;
+
+	for (int i=0; str[i] != '\0'; i++) {
+		if(isdigit(str[i])){
+			hasDigs = 1;
+			int curr = (str[i]-'0');
+			if((res==INT_MAX/10 && (sign<0 && (curr>-(INT_MIN%10)) || sign>0 && (curr>(INT_MAX%10))))||res>INT_MAX/10){
+				 assert(0==1);		 
+			}
+			res = res*10 + curr;
+		}
+		if(str[i]=='-'){
+			sign*=-1;
+		}
+	}
+
+	assert(hasDigs==1);
+
+	return res*sign;
 }
