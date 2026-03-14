958afc3c94b306ce8e2ea34a0b5eac28d9697a10
Alzzzu <alisa.zagorodnikova@yandex.ru>
2026-03-14 15:01:32 +0300

    implementation with dumb capacity

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -20,9 +20,11 @@ class simple_basic_string
 	/// Конструктор по умолчанию
 	simple_basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
-	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size) {
-		for (size_t i=0;i<size_;i++){
-			ptr_[i]  =' ';
+	simple_basic_string(size_t size) : ptr_(new T[size + 1]), size_(size)
+	{
+		for (size_t i = 0; i < size_; i++)
+		{
+			ptr_[i] = ' ';
 		}
 		ptr_[size_] = '\0';
 	}
@@ -30,40 +32,45 @@ class simple_basic_string
 	simple_basic_string(std::initializer_list<T> il)
 		: ptr_(new T[il.size() + 1]), size_(il.size())
 	{
-		for(size_t i =0; i<size_;i++){
-			ptr_[i] =*(il.begin()+i);
+		for (size_t i = 0; i < size_; i++)
+		{
+			ptr_[i] = *(il.begin() + i);
 		}
-		ptr_[size_] ='\0';
+		ptr_[size_] = '\0';
 	}
 
 	/// Конструктор с параметром си-с
-	simple_basic_string(const T* c_str) {
+	simple_basic_string(const T* c_str)
+	{
 		size_ = strlen_(c_str);
-		ptr_ = new T[size_+1];
-		for(size_t i = 0;i<=size_;i++){
-			ptr_[i] = c_str[i]; 
+		ptr_ = new T[size_ + 1];
+		for (size_t i = 0; i <= size_; i++)
+		{
+			ptr_[i] = c_str[i];
 		}
 	}
 
 	/// Конструктор копирования
-	simple_basic_string(const simple_basic_string& other) {
+	simple_basic_string(const simple_basic_string& other)
+	{
 		size_ = other.size_;
-		ptr_ = new T[size_+1];
-		for (size_t i =0; i<=size_;i++){
+		ptr_ = new T[size_ + 1];
+		for (size_t i = 0; i <= size_; i++)
+		{
 			ptr_[i] = other.ptr_[i];
 		}
 	}
 
 	/// Перемещающий конструктор
-	simple_basic_string(simple_basic_string&& dying) : size_(dying.size_), ptr_(dying.ptr_) {
+	simple_basic_string(simple_basic_string&& dying)
+		: size_(dying.size_), ptr_(dying.ptr_)
+	{
 		dying.ptr_ = nullptr;
 		dying.size_ = 0;
 	}
 
 	/// Деструктор
-	~simple_basic_string() {
-		clean_();
-	}
+	~simple_basic_string() { clean_(); }
 
 	/// Геттер на си-строку
 	const T* c_str() const { return ptr_; }
@@ -71,53 +78,60 @@ class simple_basic_string
 	size_t size() const { return size_; }
 
 	/// Оператор перемещающего присваивания
-	simple_basic_string& operator=(simple_basic_string&& other) { 		
-		if(this==&other) return *this;
+	simple_basic_string& operator=(simple_basic_string&& other)
+	{
+		if (this == &other)
+			return *this;
 		delete[] ptr_;
 		size_ = other.size_;
 		ptr_ = other.ptr_;
 		other.ptr_ = new T[1]{'\0'};
 		other.size_ = 0;
-		return *this; 
+		return *this;
 	}
 
 	/// Оператор копирующего присваивания си строки
-	simple_basic_string& operator=(const T* c_str) {
+	simple_basic_string& operator=(const T* c_str)
+	{
 		delete[] ptr_;
 		size_ = strlen_(c_str);
-		T* new_ptr_ = new T[size_+1];
-		for(size_t i = 0;i<= size_;i++){
-			new_ptr_[i] = c_str[i]; 
+		T* new_ptr_ = new T[size_ + 1];
+		for (size_t i = 0; i <= size_; i++)
+		{
+			new_ptr_[i] = c_str[i];
 		}
-		ptr_=new_ptr_;
-		return *this; 
+		ptr_ = new_ptr_;
+		return *this;
 	}
 
 	/// Оператор копирующего присваивания
-	simple_basic_string& operator=(const simple_basic_string& other) { 
-		if(this==&other) return *this;
+	simple_basic_string& operator=(const simple_basic_string& other)
+	{
+		if (this == &other)
+			return *this;
 		delete[] ptr_;
 		size_ = other.size_;
-		T* new_ptr_ = new T[size_+1];
-		for (size_t i =0; i<= size_;i++){
+		T* new_ptr_ = new T[size_ + 1];
+		for (size_t i = 0; i <= size_; i++)
+		{
 			new_ptr_[i] = other.ptr_[i];
 		}
 		ptr_ = new_ptr_;
-		return *this; 
+		return *this;
 	}
 
 	friend simple_basic_string<T> operator+(const simple_basic_string<T>& left,
 											const simple_basic_string<T>& right)
 	{
 		simple_basic_string new_string(left);
-		new_string+=right;
+		new_string += right;
 		return new_string;
 	}
 
 	template <typename S>
 	friend S& operator<<(S& os, const simple_basic_string& obj)
 	{
-		os<<obj.c_str();
+		os << obj.c_str();
 		return os;
 	}
 
@@ -125,63 +139,74 @@ class simple_basic_string
 	friend S& operator>>(S& is, simple_basic_string& obj)
 	{
 		T ch;
-		while(is.get(ch)){
-			obj+=ch;
+		while (is.get(ch))
+		{
+			obj += ch;
 		}
 		return is;
 	}
 
-	simple_basic_string& operator+=(const simple_basic_string& other) { 
-		T* new_ptr_ = new T[size_+other.size_+1];
-		for(size_t i =0; i<size_;i++){
+	simple_basic_string& operator+=(const simple_basic_string& other)
+	{
+		T* new_ptr_ = new T[size_ + other.size_ + 1];
+		for (size_t i = 0; i < size_; i++)
+		{
 			new_ptr_[i] = ptr_[i];
 		}
-		for(size_t j = 0; j<=other.size_;j++){
-			new_ptr_[j+size_] = other.ptr_[j];
+		for (size_t j = 0; j <= other.size_; j++)
+		{
+			new_ptr_[j + size_] = other.ptr_[j];
 		}
 		size_ += other.size_;
 		delete[] ptr_;
 		ptr_ = new_ptr_;
-		return *this; 
+		return *this;
 	}
 
-	simple_basic_string& operator+=(T symbol) { 
-		T* new_ptr_ = new T[size_+2];
-		for(size_t i =0; i<size_;i++){
+	simple_basic_string& operator+=(T symbol)
+	{
+		T* new_ptr_ = new T[size_ + 2];
+		for (size_t i = 0; i < size_; i++)
+		{
 			new_ptr_[i] = ptr_[i];
 		}
 		new_ptr_[size_] = symbol;
-		new_ptr_[size_+1] = '\0';
+		new_ptr_[size_ + 1] = '\0';
 		delete[] ptr_;
 		ptr_ = new_ptr_;
 		size_++;
-		return *this; 
+		return *this;
 	}
 
 	T& operator[](size_t index) noexcept { return *(ptr_ + index); }
 
-	T& at(size_t index) { 
-		if(index<0||index>=size_){
-		throw std::out_of_range("Wrong index"); 
+	T& at(size_t index)
+	{
+		if (index < 0 || index >= size_)
+		{
+			throw std::out_of_range("Wrong index");
 		}
-		return ptr_[index]; 
+		return ptr_[index];
 	}
 
 	T* data() { return ptr_; }
 
    private:
-	static size_t strlen_(const T* str) { 
+	static size_t strlen_(const T* str)
+	{
 		size_t len = 0;
-		while (str[len]!='\0'){
+		while (str[len] != '\0')
+		{
 			len++;
 		}
-		return len; 
+		return len;
 	}
 
-	void clean_() {
+	void clean_()
+	{
 		delete[] ptr_;
-		ptr_ =nullptr;
-		size_=0;
+		ptr_ = nullptr;
+		size_ = 0;
 	}
 
 	T* ptr_ = new T[1]{'\0'};
--- tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
+++ tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <cstring>
 #include <exception>
 #include <iostream>
-#include <cstring>
 
 namespace bmstu
 {
@@ -45,183 +45,211 @@ class basic_string
 
 	bool is_long() const { return is_long_; }
 
-	T* get_ptr() { return is_long_?data_.long_str.ptr:data_.short_str.buffer; }
+	T* get_ptr()
+	{
+		return is_long_ ? data_.long_str.ptr : data_.short_str.buffer;
+	}
 
-	const T* get_ptr() const { return is_long_?data_.long_str.ptr:data_.short_str.buffer; }
+	const T* get_ptr() const
+	{
+		return is_long_ ? data_.long_str.ptr : data_.short_str.buffer;
+	}
 
-	size_t get_size() const { return is_long_?data_.long_str.size:data_.short_str.size; }
+	size_t get_size() const
+	{
+		return is_long_ ? data_.long_str.size : data_.short_str.size;
+	}
 
-	size_t get_capacity() const { 
-		return is_long_? data_.long_str.capacity: SSO_CAPACITY; 
+	size_t get_capacity() const
+	{
+		return is_long_ ? data_.long_str.capacity : SSO_CAPACITY;
 	}
 
    public:
-	basic_string() {
+	basic_string()
+	{
 		is_long_ = false;
-		data_.short_str.buffer[0]='\0';
-		data_.short_str.size=0;
+		data_.short_str.buffer[0] = T();
+		data_.short_str.size = 0;
 	}
 
-	basic_string(size_t size) {	
-		if(size>SSO_CAPACITY){
+	basic_string(size_t size)
+	{
+		if (size > SSO_CAPACITY)
+		{
 			is_long_ = true;
 			data_.long_str.capacity = size + 1;
 			data_.long_str.size = size;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
-		else{
+		else
+		{
 			is_long_ = false;
 			data_.short_str.size = size;
 		}
-		for(size_t i = 0; i < size; i++){
+		for (size_t i = 0; i < size; i++)
+		{
 			get_ptr()[i] = ' ';
 		}
-		get_ptr()[size] = '\0';
+		get_ptr()[size] = T();
 	}
 
-	basic_string(std::initializer_list<T> il) {
+	basic_string(std::initializer_list<T> il)
+	{
 		size_t size = il.size();
-		if(size > SSO_CAPACITY){
+		if (size > SSO_CAPACITY)
+		{
 			is_long_ = true;
 			data_.long_str.capacity = size + 1;
 			data_.long_str.size = size;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
-		else{
-			is_long_=false;
+		else
+		{
+			is_long_ = false;
 			data_.short_str.size = size;
 		}
 		size_t i = 0;
-		for(T val: il){
+		for (T val : il)
+		{
 			get_ptr()[i] = val;
 			i++;
 		}
-		get_ptr()[size] = '\0';
+		get_ptr()[size] = T();
 	}
 
-	basic_string(const T* c_str) {
-	
-		size_t len = strlen_(c_str);	
-	
-		if(len>SSO_CAPACITY){
+	basic_string(const T* c_str)
+	{
+		size_t len = strlen_(c_str);
+
+		if (len > SSO_CAPACITY)
+		{
 			is_long_ = true;
 			data_.long_str.capacity = len + 1;
 			data_.long_str.size = len;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
-		else {
-			is_long_=false;
+		else
+		{
+			is_long_ = false;
 			data_.short_str.size = len;
 		}
-		
-		for(size_t i = 0; i<=len; i++){
+
+		for (size_t i = 0; i <= len; i++)
+		{
 			get_ptr()[i] = c_str[i];
 		}
-
 	}
 
-	basic_string(const basic_string& other) {
-		
-		is_long_ = other.is_long();
+	basic_string(const basic_string& other)
+	{
+		is_long_ = other.is_long_;
 
-		if (is_long()){
+		if (is_long_)
+		{
 			data_.long_str.capacity = other.data_.long_str.capacity;
 			data_.long_str.size = other.data_.long_str.size;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
 
-		else{
+		else
+		{
 			data_.short_str.size = other.data_.short_str.size;
 		}
-		
-		for(size_t i = 0; i<=size(); i++){
+		for (size_t i = 0; i <= size(); i++)
+		{
 			get_ptr()[i] = other.get_ptr()[i];
 		}
 	}
 
-	basic_string(basic_string&& dying) noexcept {
-		is_long_= dying.is_long();
-		dying.is_long_ = false;
+	basic_string(basic_string&& dying) noexcept
+	{
+		is_long_ = false;
+		std::swap(is_long_, dying.is_long_);
 		std::swap(data_, dying.data_);
 	}
 
-	~basic_string() {clean_();}
+	~basic_string() { clean_(); }
 
-	const T* c_str() const { 
-		return get_ptr();
-	}
+	const T* c_str() const { return get_ptr(); }
 
-	size_t size() const { return get_size(); } 
+	size_t size() const { return get_size(); }
 
 	bool is_using_sso() const { return !is_long_; }
 
 	size_t capacity() const { return get_capacity(); }
 
-	basic_string& operator=(basic_string&& other) noexcept { 
+	basic_string& operator=(basic_string&& other) noexcept
+	{
 		clean_();
-		other.is_long_ = false;
+		is_long_ = false;
+		std::swap(is_long_, other.is_long_);
 		std::swap(data_, other.data_);
-		return *this; 
+		return *this;
 	}
 
-	basic_string& operator=(const T* c_str) { 
-	
+	basic_string& operator=(const T* c_str)
+	{
 		clean_();
-		
 		size_t len = strlen_(c_str);
-		
-		if(len > SSO_CAPACITY){
+
+		if (len > SSO_CAPACITY)
+		{
 			is_long_ = true;
 			data_.long_str.capacity = len + 1;
 			data_.long_str.size = len;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
 
-		else{
-			is_long_=false;
+		else
+		{
+			is_long_ = false;
 			data_.short_str.size = len;
 		}
 
-		for(size_t i = 0; i<=len; i++){
+		for (size_t i = 0; i <= len; i++)
+		{
 			get_ptr()[i] = c_str[i];
 		}
 
-		return *this; 
+		return *this;
 	}
 
-	basic_string& operator=(const basic_string& other) { 
-		
+	basic_string& operator=(const basic_string& other)
+	{
 		clean_();
+		is_long_ = other.is_long_;
 
-		is_long_ = other.is_long();
-		
-		if (is_long_){
+		if (is_long_)
+		{
 			data_.long_str.capacity = other.data_.long_str.capacity;
-			data_.long_str.size = other.data_.long_str.capacity;
+			data_.long_str.size = other.data_.long_str.size;
 			data_.long_str.ptr = new T[data_.long_str.capacity];
 		}
-		
-		else data_.short_str.size = other.data_.short_str.size;
 
-		for (size_t i = 0; i<=size(); i++){
+		else
+			data_.short_str.size = other.data_.short_str.size;
+
+		for (size_t i = 0; i <= size(); i++)
+		{
 			get_ptr()[i] = other.get_ptr()[i];
 		}
-		
-		return *this; 
+
+		return *this;
 	}
 
 	friend basic_string<T> operator+(const basic_string<T>& left,
 									 const basic_string<T>& right)
 	{
 		basic_string new_string = basic_string(left);
-		new_string+=right;
+		new_string += right;
 		return new_string;
 	}
 
 	template <typename S>
 	friend S& operator<<(S& os, const basic_string& obj)
 	{
-		os<<obj.c_str();
+		os << obj.c_str();
 		return os;
 	}
 
@@ -229,90 +257,102 @@ class basic_string
 	friend S& operator>>(S& is, basic_string& obj)
 	{
 		T ch;
-		while(is.get(ch)){
-			obj+=ch;
-		}			
+		while (is.get(ch))
+		{
+			obj += ch;
+		}
 		return is;
 	}
 
-	basic_string& operator+=(const basic_string& other) { 
-	
-		size_t new_size = size()+other.size();
-	
-		if(new_size>get_capacity()){
-			T* new_ptr= new T[new_size+1];
-			for(size_t i=0; i<size(); i++){
+	basic_string& operator+=(const basic_string& other)
+	{
+		size_t new_size = size() + other.size();
+
+		if (new_size > get_capacity())
+		{
+			T* new_ptr = new T[new_size + 1];
+			for (size_t i = 0; i < size(); i++)
+			{
 				new_ptr[i] = get_ptr()[i];
 			}
 			clean_();
 			std::swap(data_.long_str.ptr, new_ptr);
-			data_.long_str.capacity = new_size+1;
+			data_.long_str.capacity = new_size + 1;
 			data_.long_str.size = new_size;
 			is_long_ = true;
 		}
-		
-		for (size_t i = 0; i<=other.size(); i++){
-			get_ptr()[new_size-other.size()+i] =other.get_ptr()[i];
+
+		for (size_t i = 0; i <= other.size(); i++)
+		{
+			get_ptr()[new_size - other.size() + i] = other.get_ptr()[i];
 		}
-		return *this; 
+		return *this;
 	}
 
-	basic_string& operator+=(T symbol) { 
-		
-		size_t new_size = size()+1;
+	basic_string& operator+=(T symbol)
+	{
+		size_t new_size = size() + 1;
 
-		if(size()+1>get_capacity()){		
-			T* new_ptr = new T[new_size+1];	
-			for(size_t i=0;i<size();i++){
+		if (size() + 1 > get_capacity())
+		{
+			T* new_ptr = new T[new_size + 1];
+			for (size_t i = 0; i < size(); i++)
+			{
 				new_ptr[i] = get_ptr()[i];
 			}
 			clean_();
 			std::swap(data_.long_str.ptr, new_ptr);
-			data_.long_str.capacity = new_size+1;
+			data_.long_str.capacity = new_size + 1;
 			is_long_ = true;
 		}
-		
-		get_ptr()[new_size-1]=symbol;
-		get_ptr()[new_size]='\0';
 
-		if(is_long()) data_.long_str.size = new_size;
-		else data_.short_str.size = new_size;
-		return *this; 
+		get_ptr()[new_size - 1] = symbol;
+		get_ptr()[new_size] = T();
+
+		if (is_long())
+			data_.long_str.size = new_size;
+		else
+			data_.short_str.size = new_size;
+		return *this;
 	}
 
-	T& operator[](size_t index) noexcept
-	{		
-		return get_ptr()[index];
-	}
+	T& operator[](size_t index) noexcept { return get_ptr()[index]; }
 
-	T& at(size_t index) {
-		if (index>=size()){
-			throw std::out_of_range("Wrong index"); 
+	T& at(size_t index)
+	{
+		if (index >= size())
+		{
+			throw std::out_of_range("Wrong index");
 		}
 		return get_ptr()[index];
 	}
 
-	T* data() { return get_ptr();}
+	T* data() { return get_ptr(); }
 
    private:
-	static size_t strlen_(const T* str) { 
+	static size_t strlen_(const T* str)
+	{
 		size_t len = 0;
-		while (str[len]!=T()){
+		while (str[len] != T())
+		{
 			len++;
 		}
-		return len; 
+		return len;
 	}
 
-	void clean_() {
-		if(is_long_){
+	void clean_()
+	{
+		if (is_long_)
+		{
 			delete[] data_.long_str.ptr;
 			data_.long_str.ptr = nullptr;
 			data_.long_str.size = 0;
 			data_.long_str.capacity = 0;
 		}
-		else{
-			data_.short_str.buffer[0]='\0';
-			data_.short_str.size =0;
+		else
+		{
+			data_.short_str.buffer[0] = T();
+			data_.short_str.size = 0;
 		}
 	}
 };
